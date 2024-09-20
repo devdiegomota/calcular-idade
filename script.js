4046076212ce222532cccafd4859166a9429df63
Diego <diego.motasocial@gmail.com>
2024-09-20 13:01:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,38 @@ function verificar() {
         var idade = ano - Number(caixaano.value)
         var feminino = document.getElementById('feminino')
         var masculino = document.getElementById('masculino')
+        var foto = document.createElement('img')
+        foto.setAttribute('id' , 'foto')
+
         if (feminino.checked) {
             genero = 'Mulher'
+
+            if (idade >= 0 && idade < 15) {
+                //JOVEM                  
+            } else if (idade < 25) {
+                //MULHER
+            } else if (idade < 35 ) {
+                //MULHER
+            } else if (idade < 45) {
+                //MULHER
+            }
+
         } else if (masculino.checked) {
             genero = 'Homem'
+            
+            if (idade >= 0 && idade < 15) {
+                //JOVEM     
+                foto.setAttribute('src', 'img/10h.png')
+            } else if (idade < 25) {
+                //HOMEM
+            } else if (idade < 35 ) {
+                //HOMEM
+            } else if (idade < 45) {
+                //HOMEM
+            }
         }
         resultado.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos de idade`
+        resultado.appendChild(foto)
+    
     }
 }
